931b58e612c5886f835e83e0a0275545a969596a
Jennifer Nerlich <Jennifer.Nerlich@vogella.com>
2024-01-17 16:51:58 +0100

    add picture

--- math_pdf_generator/lib/choose.dart
+++ math_pdf_generator/lib/choose.dart
@@ -15,72 +15,77 @@ class _ExerciseSelectionPageState extends State<ExerciseSelectionPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          // Image.asset('/assets/images/logo.jpeg'),
-          Container(
-            padding: const EdgeInsets.all(40),
-            child: Text(
-              "Das ist eine App um Matheaufgaben zu erstellen und zu drucken",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/ic_launcher.png'),
+            Container(
+              padding: const EdgeInsets.all(40),
+              child: Text(
+                "Das ist eine App um Matheaufgaben zu erstellen und zu drucken",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
 
-          SizedBox(
-            height: 50,
-          ),
-          DropdownMenu(
-            width: 250,
-            label: Text('Level'),
-            helperText: 'Select your difficulty level',
-            leadingIcon: Icon(Icons.add_chart),
-            initialSelection: 0,
-            dropdownMenuEntries: <DropdownMenuEntry<int>>[
-              DropdownMenuEntry(value: 0, label: 'easy'),
-              DropdownMenuEntry(value: 1, label: 'medium'),
-              DropdownMenuEntry(value: 2, label: 'hard'),
-              DropdownMenuEntry(value: 3, label: 'superhero'),
-            ],
-            onSelected: (value) {
-              setState(() {});
-              level = value!;
-            },
-          ),
-          //Padding(padding: EdgeInsets.all(12)),
-          SizedBox(
-            height: 50,
-          ),
-          DropdownMenu(
-            width: 250,
-            label: Text('Nr. Pages'),
-            helperText: 'Select the number of pages',
-            leadingIcon: Icon(Icons.assignment_add),
-            initialSelection: 0,
-            dropdownMenuEntries: <DropdownMenuEntry<int>>[
-              DropdownMenuEntry(value: 0, label: '1'),
-              DropdownMenuEntry(value: 1, label: '2'),
-              DropdownMenuEntry(value: 2, label: '3'),
-              DropdownMenuEntry(value: 3, label: '4'),
-            ],
-            onSelected: (value) {
-              setState(() {});
-              pages = value!;
-            },
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              MyHomePage();
-            },
-            child: Text("Look at the preview"),
-          ),
-          // IconButton(onPressed: () {}, icon: Icon(Icons.gamepad_outlined)),
-        ],
+            SizedBox(
+              height: 30,
+            ),
+            DropdownMenu(
+              width: 250,
+              label: Text('Level'),
+              helperText: 'Select your difficulty level',
+              leadingIcon: Icon(Icons.add_chart),
+              initialSelection: 0,
+              dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                DropdownMenuEntry(value: 0, label: 'easy'),
+                DropdownMenuEntry(value: 1, label: 'medium'),
+                DropdownMenuEntry(value: 2, label: 'hard'),
+                DropdownMenuEntry(value: 3, label: 'superhero'),
+              ],
+              onSelected: (value) {
+                setState(() {});
+                level = value!;
+              },
+            ),
+            //Padding(padding: EdgeInsets.all(12)),
+            SizedBox(
+              height: 30,
+            ),
+            DropdownMenu(
+              width: 250,
+              label: Text('Nr. Pages'),
+              helperText: 'Select the number of pages',
+              leadingIcon: Icon(Icons.assignment_add),
+              initialSelection: 0,
+              dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                DropdownMenuEntry(value: 0, label: '1'),
+                DropdownMenuEntry(value: 1, label: '2'),
+                DropdownMenuEntry(value: 2, label: '3'),
+                DropdownMenuEntry(value: 3, label: '4'),
+              ],
+              onSelected: (value) {
+                setState(() {});
+                pages = value!;
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
+              child: Text("Look at the preview"),
+            ),
+            // IconButton(onPressed: () {}, icon: Icon(Icons.gamepad_outlined)),
+          ],
+        ),
       ),
     );
   }
